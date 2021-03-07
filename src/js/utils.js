0ff9ed728521966e5b3839e3f4d2444683cbdaea
DoParkEQ <do.park@eqworks.com>
2021-03-07 04:11:31 -0500

    directory change

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,8 +1,8 @@
-export function add(v1, v2) {
+function add(v1, v2) {
     return { x: v1.x + v2.x, y: v1.y + v2.y }
 }
 
-export function limit(v1, limit) {
+function limit(v1, limit) {
     
     const min = limit * -1
     const max = limit * 1;
@@ -13,25 +13,25 @@ export function limit(v1, limit) {
     }
 }
 
-export function setMag(v1, num) {
+function setMag(v1, num) {
     return {x: v1.x * num, y: v1.y * num}
 }
 
-export function sub(v1, v2) {
+function sub(v1, v2) {
     return { x: v1.x - v2.x, y: v1.y - v2.y }
 } 
 
-export function decay(v1) {
+function decay(v1) {
     const nx = v1.x > 0 ? 1 : -1;
     const ny = v1.y > 0 ? 1 : -1;
     const rate = 0.006;
     return { x: v1.x - rate * nx, y: v1.y - rate * ny }
 }
 
-export function getRandom(min, max) {
+function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-export function map(value, low1, high1, low2, high2) {
+function map(value, low1, high1, low2, high2) {
     return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
 }
